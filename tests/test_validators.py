d88d25b5b6871fe57cbab36b60567f6463116f2f
Martin Massera <martinmassera@gmail.com>
2026-05-10 01:27:02 -0300

    cleaning test

--- tests/test_validators.py
+++ tests/test_validators.py
@@ -9,6 +9,11 @@ from newsletter.validators import validate_email_nouser
 
 class ValidatorTestCase(TestCase):
     """ Test case for validators. """
+    def setUp(self):
+        super().setUp()
+        self.user = get_user_model().objects.create_user(
+            'john', 'lennon@thebeatles.com', get_random_string(length=16))
+        self.user.save()
 
     def test_validate_email_nouser_noerror(self):
         """ Test validate_email_nouser where no error is raised. """
@@ -16,20 +21,10 @@ class ValidatorTestCase(TestCase):
 
     def test_validate_email_nouser_error(self):
         """ Test validate_email_nouser where error is raised. """
-        User = get_user_model()
-        password = get_random_string(length=16)
-        user = User.objects.create_user(
-            'john', 'lennon@thebeatles.com', password)
-        user.save()
-
         with self.assertRaises(ValidationError):
-            validate_email_nouser(user.email)
+            validate_email_nouser(self.user.email)
 
     @override_settings(NEWSLETTER_VALIDATE_USER=False)
     def test_validate_email_nouser_disabled(self):
         """ With NEWSLETTER_VALIDATE_USER=False the validator is a no-op. """
-        User = get_user_model()
-        password = get_random_string(length=16)
-        User.objects.create_user('john', 'lennon@thebeatles.com', password)
-
         validate_email_nouser('lennon@thebeatles.com')
